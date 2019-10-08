d880b67c597f5afab8e58711a0d781da0077164e
KIMBALL Charles <charleskimball2012@u.northwestern.edu>
2019-10-08 13:32:13 -0500

    added options for batch submissions

--- frac_funcs.py
+++ frac_funcs.py
@@ -2,8 +2,8 @@ import numpy as np
 import precession as pr
 import argparse
 import itertools
-import pdb
 import pandas as pd
+import os
 ####### Parse Arguments #######
 
 parser = argparse.ArgumentParser()
@@ -22,6 +22,8 @@ parser.add_argument('-maxkick','--maxkick', action='store_true')
 parser.add_argument('-Na','--Na', type = int, default = 10)
 parser.add_argument('-Nq','--Nq', type = int, default = 10)
 parser.add_argument('-l','--label',type = str, default='')
+parser.add_argument('-Nbatch','--Nbatch',type = int, default = 1)
+parser.add_argument('-batch','--batch',type=int,default=1)
 ###############################
 
 def _get_kicks(amag1,amag2,q,Nsamps,maxkick):
@@ -54,13 +56,14 @@ def retention_fraction(a1a2q,vescs,Nsamps,maxkick=True):
     return float(len(np.where(v_kicks<
                               vesc_samples)[0]))/len(v_kicks),v_mean,v_median
     
-def run_retention_grid(Na, Nq, vescs, Nsamps, maxkick):
+def run_retention_grid(Na, Nq, vescs, Nsamps, Nbatch, batch, maxkick):
     a1s = np.linspace(0,1,Na)
     a2s = np.linspace(0,1,Na)
     qs = np.linspace(.001,1,Nq)
     
-    A1A2Q =list(itertools.product(a1s,a2s,qs))
- #   A1A2Q = np.array(A1A2Q)    
+    A1A2Q_all =list(itertools.product(a1s,a2s,qs))
+    batchsize = len(A1A2Q_all)/Nbatch
+    A1A2Q = A1A2Q_all[batch*batchsize:(batch+1)*batchsize]
     for ii in range(len(A1A2Q)):
         print(ii)
         rf, v_mean, v_median = retention_fraction(A1A2Q[ii],vescs,Nsamps,maxkick)
@@ -72,13 +75,16 @@ if __name__ == '__main__':
     args= parser.parse_args()
     
     tag = '_'.join(args.random_locations_file.split('_')[-4:])
+    outdir = args.out_directory+'/'+args.label+'_'+tag
+    if not os.path.isdir(outdir):
+        os.system('mkdir '+outdir)
     radius_str = tag.split('_')[1]
     mass_str = tag.split('_')[3]
     v_escs = pd.read_csv(args.random_locations_file).v_esc.values
     print('Running cluster model  with Plummer radius {0} and  mass {1} \n{2} samples per grid-node and maxkick = {3}'.format(radius_str,mass_str,args.Nsamps,args.maxkick))
     
-    fracs = run_retention_grid(args.Na,args.Nq,v_escs,args.Nsamps,args.maxkick)
-    np.savetxt(args.out_directory+'/retention_fractions_'+args.label+tag,fracs)
+    fracs = run_retention_grid(args.Na,args.Nq,v_escs,args.Nsamps,args.Nbatch,args.batch,args.maxkick)
+    np.savetxt(outdir+'/retention_fractions_'+str(args.batch),fracs)
 
 
 
